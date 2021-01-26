6ebb06cc51481eafdd48c75eff41bdfcccee502f
guamil <gustavo.j.amil@gmail.com>
2021-01-26 19:36:29 -0300

    corregí el nombre del html a index

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.use( express.static (publicPath) );
 app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
 })
