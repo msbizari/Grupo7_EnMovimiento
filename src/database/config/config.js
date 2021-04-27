6f2f8cd93bf19f62c1dbc557cec16e8e35f30adb
msbizari <msbizari@gmail.com>
2021-04-27 18:58:07 -0300

    correccion nombre de la base de datos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "product_db",
+    "database": "products_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
