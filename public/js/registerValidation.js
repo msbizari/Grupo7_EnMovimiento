249ee0ee299e768e257f3199e0d90b881eaf0dff
guamil <gustavo.j.amil@gmail.com>
2021-05-28 17:12:24 -0300

    cambios menores

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -3,29 +3,19 @@ window.onload = function(){
     form.addEventListener('submit', (e) => {
         let errors = [];
 
-        let name = document.querySelector('#Name');
         let lastName = document.querySelector('#lastName');
         let email = document.querySelector('#email');
-        let emailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
         let password = document.querySelector('#password');
-        let image = document.querySelector('#myfile');
-        let acceptedExtensions = [".jpg", ".png" , ".gif"];
-        let fileExtensions = path.extname(file.originalname);
-       
-        if (name.value == '' || name.value.length < 2 ) {
-            errors.push('El campo Apellido no puede estar vacío, debe contener al menos 2 carácteres');
-            lastName.classList.add('is-invalid')
-        };
-        
-        if (lastName.value == '' || lastName.value.length < 2 ) {
-            errors.push('El campo Apellido no puede estar vacío, debe contener al menos 2 carácteres');
+
+        if (lastName.value == '') {
+            errors.push('El campo Apellido no puede estar vacío');
             lastName.classList.add('is-invalid');
         }/*  else {
             lastName.classList.add('is-valid');
             lastName.classList.remove('is-invalid');
         } */;
-        if (email.value == '' || !email.value.includes ('@') || !email.value.match (emailFormat)) {
-            errors.push('El campo email no puede estar vacío, debe contener un email valido');
+        if (email.value == '') {
+            errors.push('El campo Apellido no puede estar vacío');
             email.classList.add('is-invalid');
         } /* else {
             email.classList.add('is-valid');
@@ -46,14 +36,9 @@ window.onload = function(){
             rating.classList.remove('is-invalid');
             form.awards.focus();
         } */;
-        if (!image.fileExtensions.includes (acceptedExtensions)){
-            errors.push('Las extensiones aceptadas son '+ acceptedExtensions.join(","));
-            image.classList.add('is-invalid');
-        };
-
         if (errors.length > 0){
             e.preventDefault();
-            
+
             let listaErrores = document.querySelector(".errors"); 
             listaErrores.innerHTML = "";  
             for(error of errors){
