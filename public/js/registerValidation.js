98e3ea745c05d8ca08d40bf595287ae81d020f4d
msbizari <msbizari@gmail.com>
2021-05-27 22:46:22 -0300

    Validacion front-end userRegister

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -6,16 +6,25 @@ window.onload = function(){
         let name = document.querySelector('#Name');
         let lastName = document.querySelector('#lastName');
         let email = document.querySelector('#email');
+        let emailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
         let password = document.querySelector('#password');
+        let image = document.querySelector('#myfile');
+        let acceptedExtensions = [".jpg", ".png" , ".gif"];
+        let fileExtensions = path.extname(file.originalname);
        
-        if (lastName.value == '') {
+        if (name.value == '' || name.value.length < 2 ) {
+            errors.push('El campo Apellido no puede estar vacío, debe contener al menos 2 carácteres');
+            lastName.classList.add('is-invalid')
+        };
+        
+        if (lastName.value == '' || lastName.value.length < 2 ) {
             errors.push('El campo Apellido no puede estar vacío');
             lastName.classList.add('is-invalid');
         }/*  else {
             lastName.classList.add('is-valid');
             lastName.classList.remove('is-invalid');
         } */;
-        if (email.value == '') {
+        if (email.value == '' || !email.value.includes ('@') || !email.value.match (emailFormat)) {
             errors.push('El campo email no puede estar vacío');
             email.classList.add('is-invalid');
         } /* else {
@@ -37,6 +46,11 @@ window.onload = function(){
             rating.classList.remove('is-invalid');
             form.awards.focus();
         } */;
+        if (!image.fileExtensions.includes (acceptedExtensions)){
+            errors.push('Las extensiones aceptadas son '+ acceptedExtensions.join(","));
+            image.classList.add('is-invalid');
+        };
+
         if (errors.length > 0){
             e.preventDefault();
             
