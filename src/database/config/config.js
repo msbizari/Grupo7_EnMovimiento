8e9463d3234f4bf7d3bf2dca1009e57c898ad811
Carolinamoscuzza <carolinamoscuzza2019@gmail.com>
2021-06-01 17:02:37 -0300

    Commiteando Edicion, Delete y Carrusel intento

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "Forexatron2019",
     "database": "en_movimiento",
     "host": "127.0.0.1",
     "dialect": "mysql"
