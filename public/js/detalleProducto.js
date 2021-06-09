2df6fab5540f2b1e5459a12ea929d0f6f833c1fc
msbizari <msbizari@gmail.com>
2021-06-09 20:57:01 -0300

    esbozo de carrito funcionando, detalles estéticos

---
+++ public/js/detalleProducto.js
@@ -0,0 +1,13 @@
+window.onload = function (){
+    document.querySelector(".boton-comprar").onclick = function (e) {
+        e.preventDefault();
+        let producto = {
+            nombre: document.querySelector (),
+
+        }
+        localStorage /*carrito, abrir array acordar de hacer json stringy para guardar y viceversa para leer el json, y meterle el producto*/
+
+    };
+};
+
+/*en la pagina del carrito propiamente dicho: levantar el localstorage guardado y hacer un for each*/
