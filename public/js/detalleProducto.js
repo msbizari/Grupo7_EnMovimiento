405980dbbffb603d0572129d94eed8f28a8c095d
guamil <gustavo.j.amil@gmail.com>
2021-06-10 17:28:56 -0300

    armado de carrito

--- public/js/detalleProducto.js
+++ public/js/detalleProducto.js
@@ -1,11 +1,34 @@
 window.onload = function (){
+    document.querySelector(".boton-borrar-carrito").onclick = function (e) {
+        localStorage.removeItem("producto")
+        console.log(localStorage.producto)
+    }
+    
     document.querySelector(".boton-comprar").onclick = function (e) {
         e.preventDefault();
-        let producto = {
-            nombre: document.querySelector (),
-
+        let product = {
+            id: Number(document.querySelector('.id-producto').innerText),
+            name: document.querySelector('.nombre-producto').innerText,
+            description: document.querySelector('.description-producto').innerText,
+            price: Number(document.querySelector('.price-producto').innerText),
+            discount: Number(document.querySelector('.discount-producto').innerText),
+            size: document.querySelector('.size-producto').innerText,
+            brand: document.querySelector('.brand-producto').innerText,
+        };
+       if(localStorage.getItem('producto')){
+           let products = JSON.parse(localStorage.getItem("producto"));
+           
+           products.push(product)
+           console.log(products)
+           localStorage.setItem("producto",JSON.stringify(products))
+        }else{
+            let productArray = [product]
+        localStorage.setItem("producto",JSON.stringify(productArray))
         }
-        localStorage /*carrito, abrir array (acordar de hacer json stringy para guardar y viceversa para leer el json)
+
+        console.log(localStorage.producto)
+        alert("se agregó este producto al carrito")
+        /*carrito, abrir array (acordar de hacer json stringy para guardar y viceversa para leer el json)
         y meterle el producto*/
 
     };
