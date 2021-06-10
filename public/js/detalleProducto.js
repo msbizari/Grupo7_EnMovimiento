dc0a408982cfe7c6b879b23d79a6140412884273
msbizari <msbizari@gmail.com>
2021-06-09 22:27:48 -0300

    subiendo cambios

--- public/js/detalleProducto.js
+++ public/js/detalleProducto.js
@@ -5,9 +5,12 @@ window.onload = function (){
             nombre: document.querySelector (),
 
         }
-        localStorage /*carrito, abrir array acordar de hacer json stringy para guardar y viceversa para leer el json, y meterle el producto*/
+        localStorage /*carrito, abrir array (acordar de hacer json stringy para guardar y viceversa para leer el json)
+        y meterle el producto*/
 
     };
 };
 
 /*en la pagina del carrito propiamente dicho: levantar el localstorage guardado y hacer un for each*/
+
+//el local storage no se destruye automaticamente, hay que poner un boton para vaciar el carrito
